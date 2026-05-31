0827c45a2255d2ef987c54d318e250bffcf7bc87
TheItcor <itcorsmail@gmail.com>
2026-06-01 00:00:37 +0300

    First commit

---
+++ bin/notaeshell.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Notaeshell - A bash-script for working with tasks and notes in the terminal.
+# Version: 0.1 beta
+#
+# Author: Itcor (TheItcor)
+# Main repository: codeberg.org/Itcor/Notaesh
+# Github-mirror: -
+# Suggestions and corrections will be extremely welcome.
+
+parse_args() {
+    POSITIONAL_ARGS=()
+
+}
+
+
+
+main() {
+
+    # ========= 0. Parse Args =========
+    # no args: just print all tasks/notes
+    parse_args "$@"
+
+    # ========= 1. Read file with tasks/notes =========
+
+    # ========= 2. If args != 0: Execute user's command =========
+
+    # ========= 3. If args != 0: Write task/notes in file =========
+
+    # ========= 4. Print tasks, notes =========
+    printf "========= Notash ==========\n"
+
+    printf "===========================\n"
+}
+
+main "$@"
